23e436a4249296b94cd693d5d706348063628d08
Louis Y <louisy603@hotmail.com>
2022-04-11 18:04:58 -0400

    commit tree 95afb863e6f17ee2242e977316500e3444c2bf50

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let para = document.getElementById('text')
+
+
+document.addEventListener('DOMContentLoaded', function() {
+        return para.textContent = 'This is really cool!'
+    })
+
+console.log(para)
